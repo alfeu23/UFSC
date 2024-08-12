3742e28d650df3abeb41896465cb659e0babfc5a
alfeu23 <pedro.alfeu23@gmail.com>
2024-08-12 14:03:07 -0300

    Adding course pages

---
+++ node - udemy/databases/models/product.js
@@ -0,0 +1,30 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('node-complete', 'root', 'senha', {
+  dialect: 'mysql',
+  host: 'localhost',
+});
+
+const Product = sequelize.define('product', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  title: Sequelize.STRING,
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false,
+  },
+  iamgeUrl: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = Product;
